46158d71dd911dd73deb9b83ffc1b5db11808be9
MarpuSiddardha <siddhu.marpu111@gmail.com>
2025-07-30 14:58:06 +0530

    Add full project with large file tracked via Git LFS

---
+++ src/test/java/com/siddardha/StartingDocker/StartingDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddardha.StartingDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartingDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
